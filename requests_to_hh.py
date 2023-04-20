2a598a0f89044d0b89e54300e9d62a6135f9c949
marie-george <maria85night@gmail.com>
2023-04-20 19:02:21 +0300

    Создание модуля с функциями, осуществляющими запросы к hh.ru

---
+++ requests_to_hh.py
@@ -0,0 +1,59 @@
+import requests
+
+
+def get_company_id(companies):
+    id_list = []
+    for i in companies:
+        response = requests.get(f"https://api.hh.ru/employers?text={i}").json()
+        company_id = response['items'][0]['id']
+        id_list.append(company_id)
+    return id_list
+
+
+def get_vacancies_link(ids):
+    vacancies_link_list = []
+    for i in ids:
+        id_response = requests.get(f"https://api.hh.ru/employers/{i}").json()
+        vacancies_url = id_response['vacancies_url']
+        vacancies_link_list.append(vacancies_url)
+    return vacancies_link_list
+
+
+def get_vacancies_number(links):
+    vacancies_number_list = []
+    for i in links:
+        vacancies_response = requests.get(f"{i}", params={'per_page': '100', 'page': 0}).json()
+        vacancies_number = 0
+        for n in vacancies_response['items']:
+            vacancies_number += 1
+        vacancies_number_list.append(vacancies_number)
+    return vacancies_number_list
+
+
+def get_salary(salary_info: dict):
+    """Обработка поля salary(зарплата): предпочтительно выводить зарплату 'от', если же она не указана,
+    то выводить зарплату 'до'. Или выводить 0, если поле отсутствует"""
+    if salary_info:
+        if salary_info.get('to'):
+            return salary_info['to']
+        if salary_info.get('from'):
+            return salary_info['from']
+    return 0
+
+
+def get_vacancy_info(links):
+    vacancies_info_list = []
+    for i in links:
+        vacancies_response = requests.get(f"{i}", params={'per_page': '100', 'page': 0}).json()
+        for vacancy in vacancies_response['items']:
+            vacancies_info_list.append({
+                "vacancy_id": vacancy['id'],
+                "company_id": vacancy['employer']['id'],
+                "vacancy_name": vacancy['name'],
+                "salary": get_salary(vacancy['salary']),
+                "link": vacancy['alternate_url'],
+                "area": vacancy['area']['name'],
+                "experience": vacancy['experience']['name'],
+                "employment": vacancy['employment']['name']
+            })
+    return vacancies_info_list
